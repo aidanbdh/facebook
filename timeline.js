b4d4a121f25561c40bff7dbea2ddc92b9877f481
aidanbdh <aidanbdh1998@gmail.com>
2016-11-22 09:21:43 -0800

    Cleaned up post js and added array for posts #1

--- timeline.js
+++ timeline.js
@@ -1,20 +1,36 @@
 console.log('We are here, we are here, we are here!')
 var timeline = function(){
+  //Initialize Variables
   var form = document.getElementById('post');
   var formtext = document.getElementById('posttext');
-  formtext.addEventListener('blur', function(e) {
+  var posts = [
+
+  ]
+  //Default textbox text
+  formtext.addEventListener('click', function(e) {
     if(formtext.value === formtext.defaultValue) {
       formtext.value = "";
     };
   }, false);
-  var addPost = function(){
-    if(formtext.value !== formtext.defaultValue) {
-      var newEl = document.createElement('p');
-      var newText = document.createTextNode(formtext.value);
-      newEl.appendChild(newText);
-      form.appendChild(newEl);
+  formtext.addEventListener('blur', function(e) {
+    if(formtext.value === "") {
+      formtext.value = formtext.defaultValue;
     };
+  }, false);
+  //Add post function
+  var addPost = function(text){
+    var newEl = document.createElement('p');
+    var newText = document.createTextNode(text);
+    newEl.appendChild(newText);
+    form.appendChild(newEl);
   };
-  form.addEventListener('submit', addPost, false);
+  //Adding existing post
+  //Writing post
+  //posts[posts.length] = formtext.value;
+  //form.addEventListener('submit', preventDefault, false); preventDefault is not defined
+  form.addEventListener('submit', function() {
+    preventDefault();
+    addPost(formtext.value);
+  }, false);
 };
 timeline();
