ec884eba1a8ff8dd1d71c91d4a755cd1000f6df9
aidanbdh <aidanbdh1998@gmail.com>
2016-11-23 10:25:20 -0800

    Added login function and displays profile info on page #9

--- profile.js
+++ profile.js
@@ -1,15 +1,18 @@
-//Dom content based on user
-//Profile Name
-document.getElementById('profilename').textContent = currentProfile.name;
-
-//Profile Photo
-
-//Cover Photo
-
-//Profile info
-document.getElementById('work').textContent = "Work: " + currentProfile.work;
-document.getElementById('school').textContent = "School: " + currentProfile.school;
-document.getElementById('home').textContent = "Home: " + currentProfile.home;
-document.getElementById('interests').textContent = "Interests: " + currentProfile.interests;
-document.getElementById('bio').textContent = "Bio: " + currentProfile.bio;
-document.getElementById('quotes').textContent = 'Quotes: "' + currentProfile.quotes + '"';
+var profileFunction = function() {
+  //Dom content based on user
+  //Profile Name
+  document.getElementById('profilename').textContent = currentProfile.name;
+  //Profile Photo
+  var photo = document.getElementById('profilephotoimg');
+  photo.src = currentProfile.profilePicture;
+  //Cover Photo
+  /*var cover = document.getElementById('cover');
+  cover.style.backgroundImage = currentProfile.coverPhoto;*/
+  //Profile info
+  document.getElementById('work').textContent = "Work: " + currentProfile.work;
+  document.getElementById('school').textContent = "School: " + currentProfile.school;
+  document.getElementById('home').textContent = "Home: " + currentProfile.home;
+  document.getElementById('interests').textContent = "Interests: " + currentProfile.interests;
+  document.getElementById('bio').textContent = "Bio: " + currentProfile.bio;
+  document.getElementById('quotes').textContent = 'Quotes: "' + currentProfile.quotes + '"';
+};
--- timeline.js
+++ timeline.js
@@ -1,5 +1,12 @@
 console.log('We are here, we are here, we are here!')
-var timeline = function(){
+var timelineFunction = function(){
+  //Clear timeline
+  /*var timeline = document.getElementById('timeline');
+  var timelineDiv = timeline.getElementsByTagName('div');
+  console.log(timelineDiv);
+  for(i=0; i<currentProfile.posts.length; i++) {
+    timeline.removeChild(timelineDiv[0]);
+  };*/
   //Initialize Variables
   var form = document.getElementById('post');
   var formtext = document.getElementById('posttext');
@@ -28,25 +35,27 @@ var timeline = function(){
   initial();
   //Add post function
   var addPost = function(){
-    //Add text to posts array
-    posts[currentProfile.posts.length] = formtext.value;
-    //Add a new post
-    var newDiv = document.createElement('div');
-    newDiv.className = "post";
-    var newEl = document.createElement('p');
-    var newText = document.createTextNode(currentProfile.posts[currentProfile.posts.length-1]);
-    newDiv.appendChild(newEl);
-    newEl.appendChild(newText);
-    var formChild = form.lastChild;
-    for (var i=0; i<currentProfile.posts.length-1; i++){
-      formChild = formChild.previousSibling;
-    }
-    form.insertBefore(newDiv,formChild);
-    //Reset form
-    formtext.value = formtext.defaultValue;
+    if(formtext.value !== formtext.defaultValue) {
+      //Add text to posts array
+      currentProfile.posts[currentProfile.posts.length] = formtext.value;
+      //Add a new post
+      var newDiv = document.createElement('div');
+      newDiv.className = "post";
+      var newEl = document.createElement('p');
+      var newText = document.createTextNode(currentProfile.posts[currentProfile.posts.length-1]);
+      newDiv.appendChild(newEl);
+      newEl.appendChild(newText);
+      var formChild = form.firstChild;
+      formChild = formChild.nextSibling;
+      formChild = formChild.nextSibling;
+      formChild = formChild.nextSibling;
+      formChild = formChild.nextSibling;
+      form.insertBefore(newDiv,formChild);
+      //Reset form
+      formtext.value = formtext.defaultValue;
+    };
   };
   //Writing a new post
   form.addEventListener('submit', function(submit){submit.preventDefault();}, false); //Prevents form reloading the page
   form.addEventListener('submit', addPost, false);
 };
-timeline();
