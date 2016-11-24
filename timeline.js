33832e11aafa10faa36a712a9a01e0664ed69519
aidanbdh <aidanbdh1998@gmail.com>
2016-11-23 16:11:22 -0800

    FIxed style and preformance issues #14

--- timeline.js
+++ timeline.js
@@ -2,56 +2,52 @@
 var formtext = document.getElementById('posttext');
 var form = document.getElementById('timeline');
 var content = document.getElementById('content');
-var timelineFunction = function(){
-  //Default textbox text
-  formtext.addEventListener('click', function(e) {
-    if(formtext.value === formtext.defaultValue) {
-      formtext.value = "";
-    };
-  }, false);
-  formtext.addEventListener('blur', function(e) {
-    if(formtext.value === "") {
-      formtext.value = formtext.defaultValue;
-    };
-  }, false);
-  var initial = function(){
-    for (var i=currentProfile.posts.length-1; i>-1; i--) {
-      var newDiv = document.createElement('div');
-      newDiv.className = "post";
-      var newEl = document.createElement('p');
-      var newText = document.createTextNode(currentProfile.posts[i]);
-      newDiv.appendChild(newEl);
-      newEl.appendChild(newText);
-      content.appendChild(newDiv);
-    };
+//Initialize Functions
+var createPost = function(text) {
+  var newDiv = document.createElement('div');
+  newDiv.className = "post";
+  var newEl = document.createElement('p');
+  var newText = document.createTextNode(text);
+  newDiv.appendChild(newEl);
+  newEl.appendChild(newText);
+  return newDiv;
+}
+var initial = function() {
+  for (var i=currentProfile.posts.length-1; i>-1; i--) {
+    content.appendChild(createPost(currentProfile.posts[i]));
   };
+};
+var timelineFunction = function() {
   initial();
-  //Add post function
-  var addPost = function(){
+  //Add and update post function
+  var addPost = function() {
     if(formtext.value !== formtext.defaultValue) {
       //Add text to posts array
       currentProfile.posts[currentProfile.posts.length] = formtext.value;
-      console.log(currentProfile.posts.length);
-      console.log(currentProfile.posts);
       //Add a new post
-      var newDiv = document.createElement('div');
-      newDiv.className = "post";
-      var newEl = document.createElement('p');
-      var newText = document.createTextNode(currentProfile.posts[currentProfile.posts.length-1]);
-      newDiv.appendChild(newEl);
-      newEl.appendChild(newText);
       var formChild = content.firstChild;
-      if(formChild) {
-        content.insertBefore(newDiv,formChild);
+      if (formChild) {
+        content.insertBefore(createPost(currentProfile.posts[currentProfile.posts.length-1]), formChild);
       } else {
-        content.appendChild(newDiv);
+        content.appendChild(createPost(currentProfile.posts[currentProfile.posts.length-1]));
       };
       //Reset form
       formtext.value = formtext.defaultValue;
-      addedPost++;
     };
   };
-  //Writing a new post
-  form.addEventListener('submit', function(submit){submit.preventDefault();}, false); //Prevents form reloading the page
-  form.addEventListener('submit', addPost, false);
+  form.removeEventListener('submit', addPost);
+  form.addEventListener('submit', addPost);
 };
+//Default textbox text
+formtext.addEventListener('click', function(e) {
+  if (formtext.value === formtext.defaultValue) {
+    formtext.value = "";
+  };
+});
+formtext.addEventListener('blur', function(e) {
+  if (formtext.value === "") {
+    formtext.value = formtext.defaultValue;
+  };
+});
+//Writing a new post
+form.addEventListener('submit', function(submit) {submit.preventDefault();}); //Prevents form reloading the page
