f90a9fb862aaf6cc8764a661136a6b4e58967266
aidanbdh <aidanbdh1998@gmail.com>
2016-12-01 16:10:15 -0800

    Added CreateProfile function 8

--- createProfile.js
+++ createProfile.js
@@ -1,6 +1,22 @@
-function CreateProfile (username) {
-  this.Username : username;
-  return function Username () {
-    
-  };
+function CreateProfile (user, password, profilePicture, coverPhoto, name, work, school, home, interests, bio, quotes) {
+  this.user = user;
+  this.password = password;
+  this.profilePicture = profilePicture;
+  this.coverPhoto = coverPhoto;
+  this.coverPosition = 'center';
+  this.name = name;
+  this.work = work;
+  this.school = school;
+  this.home = home;
+  this.interests = interests;
+  this.bio;
+  this.quotes = quotes;
+  this.posts = [];
+  this.friends = [];
+  this.followers = [];
+  this.notifications = [{
+    text = "Welcome to Cranium Literature!";
+    username = profiles[0];
+    read = false;
+  }];
 };
