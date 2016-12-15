ee6ca3f30f05bcfdb17b9d57477b7fcae447aeea
aidanbdh <aidanbdh1998@gmail.com>
2016-12-15 10:21:10 -0800

    Fixed changes #43

--- profile.js
+++ profile.js
@@ -18,58 +18,42 @@ var updateProfile = function(profile) {
   document.getElementById('quotes').textContent = "Quotes: ''" + profile.quotes + "''";
 };
 
-String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 const editProfile = function(edit) {
-  switch (edit) {
-    case 'edit':
-      let $ps = document.getElementsByTagName('p');
-      let $newPs = [];
-      for(let i = 0; i < $ps.length; i++) {$newPs.push($ps[i])};
-      $ps = $newPs.filter(element => element.getAttribute('data-profile'));
-      for(let i = $ps.length-1; i > -1; i--) {
-        $ps[i].firstChild.splitText($ps[i].id.length + 2);
-        $span = $ps[i].lastChild;
-        $label = document.createElement('label')
-        $label.textContent = $ps[i].id.capitalize() + ': ';
-        $label.setAttribute('data-profile', $span.textContent);
-        $input = document.createElement('input');
-        $input.value = $span.textContent;
-        $label.appendChild($input);
-        $ps[i].parentNode.replaceChild($label, $ps[i]);
-      };
-      event.target.dataset.profile = 'stop';
-      event.target.textContent = 'Done Editing';
-      break;
-    case 'stop':
-      let $labels = document.getElementsByTagName('label');
-      console.log($labels);
-      let $newLabels = [];
-      for(let i = 0; i < $labels.length; i++) {$newLabels.push($labels[i])};
-      $labels = $newLabels.filter(element => element.getAttribute('data-profile'));
-      console.log($labels);
-      for(let i = $labels.length-1; i > -1; i--) {
-        $p = document.createElement('p');
-        $p.textContent = $labels[i].textContent + $labels[i].lastChild.value;
-        $p.setAttribute('data-profile', $labels[i].lastChild.value);
-        $labels[i].parentNode.replaceChild($p, $labels[i]);
-        let attribute = $labels[i].textContent.toLowerCase();
-        attribute = attribute.slice(0,attribute.length-2);
-        $p.id = attribute;
-        currentProfile[attribute] = $labels[i].lastChild.value;
-      };
-      event.target.dataset.profile = 'edit';
-      event.target.textContent = 'Edit Profile';
-      break;
-    default:
-
-    break;
-  }
-}
+  if(!!edit) {
+    let $ps = document.querySelectorAll('#profile p');
+    for(let i = $ps.length-1; i > -1; i--) {
+      $ps[i].firstChild.splitText($ps[i].id.length + 2);
+      $span = $ps[i].lastChild;
+      $label = document.createElement('label')
+      $label.textContent = capitalize($ps[i].id) + ': ';
+      $input = document.createElement('input');
+      $input.value = $span.textContent;
+      $label.appendChild($input);
+      $ps[i].parentNode.replaceChild($label, $ps[i]);
+    };
+    event.target.dataset.profile = '';
+    event.target.textContent = 'Done Editing';
+  } else {
+    let $labels = document.querySelectorAll('#profile label');
+    for(let i = $labels.length-1; i > -1; i--) {
+      $p = document.createElement('p');
+      $p.textContent = $labels[i].textContent + $labels[i].lastChild.value;
+      $labels[i].parentNode.replaceChild($p, $labels[i]);
+      let attribute = $labels[i].textContent.toLowerCase();
+      attribute = attribute.slice(0,attribute.length-2);
+      $p.id = attribute;
+      currentProfile[attribute] = $labels[i].lastChild.value;
+    };
+    event.target.dataset.profile = 'edit';
+    event.target.textContent = 'Edit Profile';
+  };
+};
 
 document.addEventListener('click', () => {
-  if(currentUser !== currentProfile || !event.target.dataset.profile) return;
+  if(currentUser !== currentProfile || event.target.id !== 'edit') return;
   editProfile(event.target.dataset.profile)
 });
